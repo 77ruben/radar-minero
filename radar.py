7c4fa842ea32f409bbd49fc219378b158f9bf456
77ruben <rub.moralesm@gmail.com>
2026-03-02 15:51:00 -0300

    Update radar.py

--- radar.py
+++ radar.py
@@ -1,7 +1,14 @@
 import requests
 from bs4 import BeautifulSoup
+import os
+
+print("RADAR FREEPORT ACTIVO")
+
+TOKEN = os.environ.get("TOKEN")
+CHAT_ID = os.environ.get("CHAT_ID")
 
 URL = "https://jobs.fcx.com/South-America/go/Oportunidades-Laborales-en-Chile/8009100/"
+BASE = "https://jobs.fcx.com"
 
 headers = {
     "User-Agent": "Mozilla/5.0"
@@ -14,14 +21,20 @@ jobs = []
 
 for job in soup.find_all("a", class_="jobTitle-link"):
     title = job.get_text(strip=True)
-    link = "https://jobs.fcx.com" + job["href"]
-    job_id = job["href"].split("/")[-2]
+    link = BASE + job["href"]
+    location = job["href"].split("/")[3]  # ciudad viene en la URL
+    jobs.append(f"{title}\n{location}\n{link}\n")
 
-    jobs.append({
-        "id": job_id,
-        "title": title,
-        "link": link
-    })
+if len(jobs) == 0:
+    message = "Radar Freeport activo.\nNo se detectaron empleos."
+else:
+    message = f"🚨 FREEPORT ({len(jobs)}) 🚨\n\n" + "\n".join(jobs)
 
-for j in jobs:
-    print(j)
+telegram_url = f"https://api.telegram.org/bot{TOKEN}/sendMessage"
+
+requests.post(telegram_url, data={
+    "chat_id": CHAT_ID,
+    "text": message[:4000]
+})
+
+print("Proceso finalizado")
