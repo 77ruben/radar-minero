f4ef7352a335847762a4330d05c0bdb26cafb169
77ruben <rub.moralesm@gmail.com>
2026-03-03 03:19:18 -0300

    Update radar.py

--- radar.py
+++ radar.py
@@ -2,7 +2,7 @@ import requests
 from bs4 import BeautifulSoup
 import os
 
-print("RADAR LUNDIN MINING — CHILE")
+print("RADAR LUNDIN MINING — CHILE FILTRADO REAL")
 
 TOKEN = os.environ["TOKEN"]
 CHAT_ID = os.environ["CHAT_ID"]
@@ -12,13 +12,13 @@ HEADERS = {
 }
 
 BASE_URL = "https://jobs.lundinmining.com"
-SEARCH_URL = BASE_URL + "/tile-search-results/?q=&locationsearch=Chile&startrow={}"
+SEARCH_URL = BASE_URL + "/tile-search-results/?q=&startrow={}"
 
 def obtener_empleos_lundin():
     empleos = []
     startrow = 0
     step = 25
-    max_paginas = 3   # seguridad anti loop infinito
+    max_paginas = 3
 
     for _ in range(max_paginas):
 
@@ -38,11 +38,20 @@ def obtener_empleos_lundin():
 
         for job in jobs:
             titulo_tag = job.find("a", class_="jobTitle-link")
-            if titulo_tag:
+            location_tag = job.find("span", class_="jobLocation")
+
+            if titulo_tag and location_tag:
+
+                ubicacion = location_tag.text.strip()
+
+                # FILTRO REAL CHILE
+                if ", CL" not in ubicacion:
+                    continue
+
                 titulo = titulo_tag.text.strip()
                 link = BASE_URL + titulo_tag["href"]
 
-                registro = f"{titulo}\n{link}"
+                registro = f"{titulo}\n{ubicacion}\n{link}"
 
                 if registro not in empleos:
                     empleos.append(registro)
